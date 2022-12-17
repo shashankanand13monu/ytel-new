e85b050ac70ceba48f3b726fecf1769f8dac3565
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2022-12-17 17:48:59 +0530

    [Update] accessToken

--- lib/ytel/screens/auth/controller/user_view_controller.dart
+++ lib/ytel/screens/auth/controller/user_view_controller.dart
@@ -11,6 +11,7 @@ class user_view_controller extends GetxController {
   static final account_controller = new TextEditingController();
   static data() async{
     String url = '${StringHelper.BASE_URL}ams/v2/accounts/users/7c8693c6-976e-4324-9123-2c1d811605f9/';
+    String accessToken = StorageUtil.getString(StringHelper.ACCESS_TOKEN);
 
     try {
       print("Inside Try");
@@ -22,7 +23,7 @@ class user_view_controller extends GetxController {
             'authority' : 'api.ytel.com',
             'accept' : 'application/json, text/plain, */*',
             'accept-language' : 'en-US,en;q=0.9',
-            'authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI1NjNmZDE0My05ZmVjLTRiYWEtYTU0NS1hZjViNzkxYmJiM2UiLCJ1SGFzaCI6IjU5ZTY0MjNkNTMyY2I1ODVkODBiOTM5NDgxZGM4MWIyNzQ1OTA5MjQ5YThhZWZmNzIxNTU0MTJlZTgwNTc5MTgiLCJwQWNjdCI6IjdjODY5M2M2LTk3NmUtNDMyNC05MTIzLTJjMWQ4MTE2MDVmOSIsImFjY3QiOiI3Yzg2OTNjNi05NzZlLTQzMjQtOTEyMy0yYzFkODExNjA1ZjkiLCJwcml2cyI6Wzg2ODY3OTYzMyw1MzY4MTYwOTYsMV0sInVzZXJuYW1lIjoicWF0ZWFtQHl0ZWwuY29tIiwic2NvcGUiOiJST0xFX0FDQ0VTU19UT0tFTiIsImlzcyI6Imh0dHBzOi8veXRlbC5jb20iLCJpYXQiOjE2NzEyNzA2NTMsImV4cCI6MTY3MTI3Nzg1M30.DLE9qLdg1i0TL3QnrcSd5D_238Miq3dRdUTkSwNIB1WLERhkAywbtjVj2xjlu4MbH7jTDfP0TdRwXirXmf6K4gOVbQZX8oMzK8XJckncVWYC1ft5UwLTgeQFbT6TZWQmgJgHquzzUca6yhrs2DQmtKteVY_whM1My5nlTHTZdprDCYmIytgE7An9VNhDoSfBu9NoDLrjJhopFqumwnc9tltaDm7bGc8rc7XP1g_IypO0SBfivPn5DOzfdKcgTfOt0_SkV5lfqSbJg3vw2LbtYk501HNjRARNk1Nj-xtmWzLj920JJh7oBLImbhdbK6dxrShec7K56HK_mFwgWZmxNw',
+            'authorization' : 'Bearer $accessToken',
             'origin' : 'https://app.ytel.com',
             'referer' : 'https://app.ytel.com/',
             'sec-ch-ua' : 'Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108',
